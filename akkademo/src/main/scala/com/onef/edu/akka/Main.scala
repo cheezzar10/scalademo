7b5e3eea489023b96936a3a12e535b6a2ed4c0f4
Andrey Smirnov <andrey.smirnov@onefactor.com>
2021-03-07 20:33:25 +0300

    imported to eclipse

--- akkademo/src/main/scala/com/onef/edu/akka/Main.scala
+++ akkademo/src/main/scala/com/onef/edu/akka/Main.scala
@@ -1,7 +1,11 @@
 package com.onef.edu.akka
 
+import com.typesafe.config.{ Config, ConfigFactory }
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("started")
+
+    val config = ConfigFactory.load()
   }
 }
