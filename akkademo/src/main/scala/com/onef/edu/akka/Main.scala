a8f4830ec54dcab51d29a065702df280e074aed3
Andrey Smirnov <andrey.smirnov@onefactor.com>
2021-03-10 09:50:22 +0300

    dumping actual actor system configuration

--- akkademo/src/main/scala/com/onef/edu/akka/Main.scala
+++ akkademo/src/main/scala/com/onef/edu/akka/Main.scala
@@ -1,11 +1,25 @@
 package com.onef.edu.akka
 
-import com.typesafe.config.{ Config, ConfigFactory }
+import com.typesafe.config.{Config, ConfigFactory, ConfigValue}
+
+import java.util.{Map => JMap}
+import scala.collection.JavaConversions._
+import scala.collection.mutable
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("started")
-
     val config = ConfigFactory.load()
+
+    dumpConfig(config)
+
+
+  }
+
+  private def dumpConfig(config: Config): Unit = {
+    val confEntries: mutable.Set[JMap.Entry[String, ConfigValue]] = config.entrySet()
+
+    for (confEntry <- confEntries) {
+      println(s"configuration entry: '${confEntry.getKey}' = '${confEntry.getValue}'")
+    }
   }
 }
