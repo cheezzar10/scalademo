5db489b899ca9894e117cedba02133c319e604f0
Andrey Smirnov <andrey.smirnov@onefactor.com>
2021-03-19 11:14:28 +0300

    code formatting + optional schema creation

--- slickdemo/src/main/scala/com/onef/Car.scala
+++ slickdemo/src/main/scala/com/onef/Car.scala
@@ -1,20 +1,21 @@
 package com.onef
 
 // use postgres instead
+
 import slick.jdbc.H2Profile.api._
 
 case class Car(
-    name: String, 
-    model: String, 
-    id: Long = 0)
+                name: String,
+                model: String,
+                id: Long = 0)
 
 // TODO basically this class should be named Cars
 class CarTable(tag: Tag) extends Table[Car](tag, "car") {
-    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
 
-    def name = column[String]("name")
+  def name = column[String]("name")
 
-    def model = column[String]("model")
+  def model = column[String]("model")
 
-    override def * = (name, model, id).mapTo[Car]
+  override def * = (name, model, id).mapTo[Car]
 }
