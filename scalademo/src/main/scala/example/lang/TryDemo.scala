248cef21dbc01886ef8a204eeae2a92abbf31205
Andrey Smirnov <andrey.smirnov@onefactor.com>
2021-07-20 10:07:38 +0300

    version string parsing

--- scalademo/src/main/scala/example/lang/TryDemo.scala
+++ scalademo/src/main/scala/example/lang/TryDemo.scala
@@ -4,6 +4,18 @@ import scala.util.Try
 
 object TryDemo {
   def main(args: Array[String]): Unit = {
+    tryIntParsing()
+  }
+
+  private def tryIntParsing(): Unit = {
+    val version = "2"
+
+    val pipelineVersion = Try(version.toInt).getOrElse(1)
+
+    print("pipeline version: " + pipelineVersion)
+  }
+
+  private def tryDemo(): Unit = {
     Try {
       println("computing")
 
