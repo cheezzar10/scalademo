6c3bff70b7023da2261fa36e42e60450f65cf5f7
Andrey Smirnov <andrey.smirnov@onefactor.com>
2021-12-10 11:34:41 +0300

    project version bumper

--- scalademo/src/main/scala/example/io/DirWalker.scala
+++ scalademo/src/main/scala/example/io/DirWalker.scala
@@ -1,14 +1,17 @@
 package example.io
 
+import java.nio.charset.StandardCharsets
 import java.nio.file.attribute.BasicFileAttributes
 import java.nio.file._
-import scala.collection.JavaConverters.asScalaIteratorConverter
+import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 import scala.sys.process.Process
+import scala.util.matching.Regex
 
 object DirWalker {
   private val BuildSbtFileName: String = "build.sbt"
   private val VersionFileName: String = "version.properties"
+  private val ModuleVersionPattern: Regex = """\w+\.version=(\d+)\.(\d+)\.(\d+)""".r
 
   def main(args: Array[String]): Unit = {
     val rootDirPath = Paths.get(sys.props("user.home"), "Documents/onefactor/ml-playground")
@@ -30,18 +33,49 @@ object DirWalker {
         .map(line => Paths.get(line.split("\t")(1)))
         .toIndexedSeq
 
-    val changedProjects = changedFiles
+    val manuallyChangedVersionFiles = (changedFiles
+      .filter(path => path.getFileName.toString == VersionFileName)
+      .toSet + Paths.get("tests/version.properties")) - Paths.get("mld/batch/version.properties")
+
+    val versionFilesToChange = changedFiles
       .map { changedFile =>
         projectDirPaths.find { projectDirPath =>
           changedFile.startsWith(projectDirPath)
         }
       }
       .flatten
+      .map(_.resolve(VersionFileName))
       .toSet
+      .diff(manuallyChangedVersionFiles)
 
-    for (changedProject <- changedProjects) {
-      println(s"changed project: $changedProject")
+    println("----- manually changed version files -----")
+    for (manuallyChangedVersionFile <- manuallyChangedVersionFiles) {
+      println("manually changed version file: " + manuallyChangedVersionFile)
     }
+    println("----- manually changed version files -----")
+
+    println("----- version.properties files to change -----")
+    for (versionFileToChange <- versionFilesToChange) {
+      println(s"changed project: $versionFileToChange")
+
+      val versionFileAbsolutePath = rootDirPath.resolve(versionFileToChange)
+      val versionFileContent =
+        new String(Files.readAllBytes(versionFileAbsolutePath), StandardCharsets.UTF_8).trim
+
+      println("version file content: '" + versionFileContent + "'")
+      versionFileContent match {
+        case ModuleVersionPattern(major, minor, bugfix) =>
+          println("minor version: " + minor)
+
+          val incrementedMinorVersion = minor.toInt + 1
+          val updatedVersionFileContent = versionFileContent
+            .replace(s"$major.$minor.$bugfix", s"$major.$incrementedMinorVersion.$bugfix")
+
+          println("updated version file content: '" + updatedVersionFileContent + "'")
+          Files.write(versionFileAbsolutePath, Seq(updatedVersionFileContent).asJava)
+      }
+    }
+    println("----- version.properties files to change -----")
   }
 }
 
